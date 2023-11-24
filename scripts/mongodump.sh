dd063cd1eaf08443d9abfd22406d5b772e88146d
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2023-11-24 13:56:09 -0600

    feat: create a backup of all databases with mongodump

--- scripts/mongodump.sh
+++ scripts/mongodump.sh
@@ -2,6 +2,31 @@ if [ -f /backup/dump ]; then
     rm /backup/dump
 fi
 
-mongodump --uri="$MONGO_BACKUP_URI" --out /backup
+# Obtiene lista de bases de datos de mongodb
+databases=$(
+    mongo \
+        --username $MONGO_BACKUP_USERNAME \
+        --password $MONGO_BACKUP_PASSWORD \
+        --host "$MONGO_BACKUP_HOST" \
+        --port "$MONGO_BACKUP_PORT" \
+        --tls \
+        --tlsAllowInvalidCertificates \
+        --quiet \
+        --eval "db.getMongo().getDBNames()" | grep -vE '^{.*}$' | jq -r '.[]'
+)
 
-touch /backup/dump
+# Recorre las bases de datos y realiza los respaldos
+for db in $databases; do
+    mongodump \
+        --username "$MONGO_BACKUP_USERNAME" \
+        --password "$MONGO_BACKUP_PASSWORD" \
+        --host "$MONGO_BACKUP_HOST" \
+        --port "$MONGO_BACKUP_PORT" \
+        --db "$db" \
+        --ssl \
+        --tlsInsecure \
+        --readPreference "secondary" \
+        --out /backup
+done
+
+# touch /backup/dump
